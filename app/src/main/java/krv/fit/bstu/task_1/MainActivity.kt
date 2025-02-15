e67fcfd0f3eadf13978c17766a72b3a75dbfacaa
Regina Karakozova <regina.karakozova@mail.ru>
2025-02-16 01:29:42 +0300

    I think. It is finished

--- app/src/main/java/krv/fit/bstu/task_1/MainActivity.kt
+++ app/src/main/java/krv/fit/bstu/task_1/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
 
     var result_save: String = ""
-
+    var last_result: String = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,6 +60,7 @@ class MainActivity : AppCompatActivity() {
         btn_delet.setOnClickListener{
             text_operation.text = ""
             text_result.text = ""
+            last_result = ""
         }
 
         // operation
@@ -78,10 +79,15 @@ class MainActivity : AppCompatActivity() {
 
         }
         btn_percent.setOnClickListener{
-            val number:String = text_result.text.toString()
-            val operation: Operation = Operation(number)
-            text_result.text = operation.getPercent()
-
+            if(last_result == "" && text_result.text ==""){
+                val number:String = text_operation.text.toString()
+                val operation: Operation = Operation(number,last_result)
+                text_result.text = operation.getPercent()
+            }else{
+                val number:String = text_result.text.toString()
+                val operation: Operation = Operation(number,last_result)
+                text_result.text = operation.getPercent()
+            }
         }
         btn_division.setOnClickListener{
             text_operation.append(" / ")
@@ -96,44 +102,189 @@ class MainActivity : AppCompatActivity() {
             text_operation.append(" + ")
         }
         btn_point.setOnClickListener{
-            text_operation.append(" . ")
+            text_operation.append(".")
         }
 
         //numbers
         btn_zero.setOnClickListener{
-            text_operation.append("0")
+            if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                text_operation.append("0")
+                val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                text_result.text = operation.basicCalculate()
+                last_result = text_result.text.toString()
+            }else{
+                if (text_result.text == "" || last_result == ""){
+                    text_operation.append("0")
+                }else{
+                    text_operation.append("0")
+                    val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                    text_result.text = operation.basicCalculate()
+                    last_result = text_result.text.toString()
+                }
+            }
         }
         btn_one.setOnClickListener{
-            text_operation.append("1") }
+            if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                text_operation.append("1")
+                val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                text_result.text = operation.basicCalculate()
+                last_result = text_result.text.toString()
+            }else{
+                if (text_result.text == "" || last_result == ""){
+                    text_operation.append("1")
+                }else{
+                    text_operation.append("1")
+                    val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                    text_result.text = operation.basicCalculate()
+                    last_result = text_result.text.toString()
+                }
+            }
+        }
         btn_two.setOnClickListener{
-            text_operation.append("2")
+            if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                text_operation.append("2")
+                val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                text_result.text = operation.basicCalculate()
+                last_result = text_result.text.toString()
+            }else{
+                if (text_result.text == "" || last_result == ""){
+                    text_operation.append("2")
+                }else{
+                    text_operation.append("2")
+                    val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                    text_result.text = operation.basicCalculate()
+                    last_result = text_result.text.toString()
+                }
+            }
        }
         btn_three.setOnClickListener{
-            text_operation.append("3") }
+            if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                text_operation.append("3")
+                val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                text_result.text = operation.basicCalculate()
+                last_result = text_result.text.toString()
+            }else{
+                if (text_result.text == "" || last_result == ""){
+                    text_operation.append("3")
+                }else{
+                    text_operation.append("3")
+                    val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                    text_result.text = operation.basicCalculate()
+                    last_result = text_result.text.toString()
+                }
+            }
+        }
         btn_fore.setOnClickListener{
-            text_operation.append("4")
+            if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                text_operation.append("4")
+                val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                text_result.text = operation.basicCalculate()
+                last_result = text_result.text.toString()
+            }else{
+                if (text_result.text == "" || last_result == ""){
+                    text_operation.append("4")
+                }else{
+                    text_operation.append("4")
+                    val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                    text_result.text = operation.basicCalculate()
+                    last_result = text_result.text.toString()
+                }
+            }
         }
         btn_five.setOnClickListener{
-            text_operation.append("5")
+            if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                text_operation.append("5")
+                val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                text_result.text = operation.basicCalculate()
+                last_result = text_result.text.toString()
+            }else{
+                if (text_result.text == "" || last_result == ""){
+                    text_operation.append("5")
+                }else{
+                    text_operation.append("5")
+                    val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                    text_result.text = operation.basicCalculate()
+                    last_result = text_result.text.toString()
+                }
+            }
        }
         btn_six.setOnClickListener{
-            text_operation.append("6")
+            if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                text_operation.append("6")
+                val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                text_result.text = operation.basicCalculate()
+                last_result = text_result.text.toString()
+            }else{
+                if (text_result.text == "" || last_result == ""){
+                    text_operation.append("6")
+                }else{
+                    text_operation.append("6")
+                    val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                    text_result.text = operation.basicCalculate()
+                    last_result = text_result.text.toString()
+                }
+            }
         }
         btn_seven.setOnClickListener{
-            text_operation.append("7")
+            if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                text_operation.append("7")
+                val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                text_result.text = operation.basicCalculate()
+                last_result = text_result.text.toString()
+            }else{
+                if (text_result.text == "" || last_result == ""){
+                    text_operation.append("7")
+                }else{
+                    text_operation.append("7")
+                    val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                    text_result.text = operation.basicCalculate()
+                    last_result = text_result.text.toString()
+                }
+            }
        }
         btn_eight.setOnClickListener{
-            text_operation.append("8")
+            if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                text_operation.append("8")
+                val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                text_result.text = operation.basicCalculate()
+                last_result = text_result.text.toString()
+            }else{
+                if (text_result.text == "" || last_result == ""){
+                    text_operation.append("8")
+                }else{
+                    text_operation.append("8")
+                    val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                    text_result.text = operation.basicCalculate()
+                    last_result = text_result.text.toString()
+                }
+            }
         }
         btn_nine.setOnClickListener{
-            text_operation.append("9") }
+             if(text_operation.text.count { it == '-' } == 1 || text_operation.text.count { it == '+' } == 1 || text_operation.text.count { it == '*' } == 1 || text_operation.text.count { it == '/' } == 1){
+                 text_operation.append("9")
+                 val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                 text_result.text = operation.basicCalculate()
+                 last_result = text_result.text.toString()
+             }else{
+                 if (text_result.text == "" || last_result == ""){
+                     text_operation.append("9")
+                 }else{
+                     text_operation.append("9")
+                     val operation: Operation = Operation(text_operation.text.toString(),last_result)
+                     text_result.text = operation.basicCalculate()
+                     last_result = text_result.text.toString()
+                 }
+             }
+
+        }
 
 
         //result
         btn_result.setOnClickListener {
-            val operation: Operation = Operation(text_operation.text.toString())
+            val operation: Operation = Operation(text_operation.text.toString(),last_result)
             text_result.text = operation.basicCalculate()
             text_operation.text = text_result.text
+            last_result = text_result.text.toString()
         }
 
     }
--- app/src/main/java/krv/fit/bstu/task_1/Operation.kt
+++ app/src/main/java/krv/fit/bstu/task_1/Operation.kt
@@ -2,40 +2,64 @@ package krv.fit.bstu.task_1
 
 import android.util.Log
 
-class Operation(val stringOperation: String) {
+class Operation(val stringOperation: String, val last_result:String) {
 
-    var numberLeft: String = ""
-    var operation: String = ""
-    var numberRight: String = ""
-    var number: String = ""
+    private var numberLeft: String = ""
+    private var operation: String = ""
+    private var numberRight: String = ""
+    private var number: String = ""
+
+    private var lastNumber: String = ""
+    private var lastOperator: String = ""
 
     //string
-    fun getParts(){
-        if (stringOperation.contains(" ")) {
-            val parts = stringOperation.split(" ")
-            numberLeft = parts[0]
-            operation = parts[1]
-            numberRight = parts[2]
-        } else {
-            number = stringOperation
-        }
+    private fun getParts(){
 
-        Log.e("SSS", "$numberLeft")
-        Log.e("SSS", "$operation")
-        Log.e("SSS", "$numberRight")
+        if(last_result == ""){
+            if (stringOperation.contains(" ")) {
+                val parts = stringOperation.split(" ")
+                numberLeft = parts[0]
+                operation = parts[1]
+                numberRight = parts[2]
+                Log.e("SSS", "$numberLeft")
+                Log.e("SSS", "$operation")
+                Log.e("SSS", "$numberRight")
+            } else {
+                number = stringOperation
+                Log.e("SSS", "$number")
+            }
+        } else{
+            if (stringOperation.contains(" ")) {
+                val parts = stringOperation.split(" ")
+                lastNumber = parts.lastOrNull() ?: ""
+                lastOperator = if (parts.size > 1) parts[parts.size - 2] else ""
+        }
+        }
 
     }
 
 
     fun basicCalculate(): String {
         getParts()
-        return when (operation) {
-            "+" -> (numberLeft.toDouble() + numberRight.toDouble()).toString()
-            "-" -> (numberLeft.toDouble() - numberRight.toDouble()).toString()
-            "*" -> (numberLeft.toDouble() * numberRight.toDouble()).toString()
-            "/" -> if (numberRight.toDouble() != 0.0) (numberLeft.toDouble() / numberRight.toDouble()).toString() else throw IllegalArgumentException("Деление на ноль")
-            else -> throw IllegalArgumentException("такой операции нет")
+        if(last_result == ""){
+            return when (operation) {
+                "+" -> (numberLeft.toDouble() + numberRight.toDouble()).toString()
+                "-" -> (numberLeft.toDouble() - numberRight.toDouble()).toString()
+                "*" -> (numberLeft.toDouble() * numberRight.toDouble()).toString()
+                "/" -> if (numberRight.toDouble() != 0.0) (numberLeft.toDouble() / numberRight.toDouble()).toString() else throw IllegalArgumentException("Деление на ноль")
+                else -> throw IllegalArgumentException("такой операции нет")
+            }
+        }else
+        {
+             return when (lastOperator) {
+                "+" -> (last_result.toDouble() + lastNumber.toDouble()).toString()
+                "-" -> (last_result.toDouble() - lastNumber.toDouble()).toString()
+                "*" -> (last_result.toDouble() * lastNumber.toDouble()).toString()
+                "/" -> if (last_result.toDouble() != 0.0) (last_result.toDouble() / lastNumber.toDouble()).toString() else throw IllegalArgumentException("Деление на ноль")
+                else -> throw IllegalArgumentException("такой операции нет")
+            }
         }
+
     }
 
     fun getPercent():  String {
